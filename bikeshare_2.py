774265ccd166880432e3a86ea924efa4d1ddf8d9
olusegun <adeyisegun@yahoo.com>
2022-04-21 18:39:13 +0100

    refactor: bikeshare_2.py

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -68,8 +68,8 @@ def get_filters():
                 print()
                 print("Sorry, I didn't understand that, kindly check your input and try again")
                 print('. '*10)
-                check = input("Please type YES to try again or type any other value to exit this program:  ")
-                if check.lower()  != 'yes':
+                check = input("Please type YES to try again or type any other value to exit this program:  ").lower()
+                if check != 'yes':
                     sys.exit("********The program has been exited successfuly********")
             else:
                 break
@@ -84,8 +84,8 @@ def get_filters():
                 print()
                 print("Sorry, I didn't understand that, kindly check your input and try again")
                 print('. '*10)
-                check = input("Please type YES to try again or type any other value to exit this program:  ")
-                if check.lower()  != 'yes':
+                check = input("Please type YES to try again or type any other value to exit this program:  ").lower()
+                if check != 'yes':
                     sys.exit("********The program has been exited successfuly********")
             else:
                 break
